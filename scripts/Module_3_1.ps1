1eb8f7bce93ba60b7be6f51c853ccb9ea7986b09
Karsten Bott <Karsten.Bott@bottnet.de>
2023-08-24 11:20:09 +0200

    Module 3_1

---
+++ scripts/Module_3_1.ps1
@@ -0,0 +1,24 @@
+$VCENTER_HOST="vcsa-7.demo.local"
+$Securestring=ConvertTo-SecureString -AsPlainText -String "Password123!" -Force
+$username="administrator@vsphere.local"
+$Credential = New-Object System.Management.Automation.PSCredential($username, $Securestring)
+$CREDS=New-PPDMcredentials
+$CREDS=New-PPDMcredentials -type VCENTER -name $username -credentials $CREDS
+Get-PPDMcertificates -newhost $VCENTER_HOST -port 443 | Approve-PPDMcertificates | out-string
+Write-Host "Adding Inventory Source $VCENTER_HOST"
+$INVENTORY_SOURCE=Add-PPDMinventory_sources -Hostname $VCENTER_HOST -port 443 -Type VCENTER -isHostingvCenter -ID $CREDS.id -Name "DEMO VCENTER"
+$DISCOVERY=$INVENTORY_SOURCE | Start-PPDMdiscoveries -level DataCopies -start inventory-sources 
+Write-Host $Discovery.name
+do { 
+    Sleep 5
+    $Activity=$DISCOVERY | Get-PPDMactivities 6>$null
+    write-host -NoNewline "$($Activity.progress)% "} 
+until ($Activity.state -eq "COMPLETED")
+Write-Host "VM Backup Settings"
+Get-PPDMvm_backup_setting | Out-String
+Write-Host "Modifying VM Backup Settings"
+$VMsettings=Get-PPDMvm_backup_setting
+$VMsettings.properties
+$VMsettings.properties[0].value=10
+$VMsettings.properties[1].value=5
+Set-PPDMvm_backup_setting -vm_backup_setting $VMsettings | Out-String
