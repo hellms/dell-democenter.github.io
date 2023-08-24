4068ec993251c8083575650a46d6c1fe0aaffac3
Karsten Bott <Karsten.Bott@bottnet.de>
2023-08-24 10:35:51 +0200

    addid module 1 script

---
+++ module1.ps1
@@ -0,0 +1,20 @@
+
+$PPDM_FQDN="ppdm-01.demo.local"
+$Securestring=ConvertTo-SecureString -AsPlainText -String "Password123!" -Force
+$username="admin"
+$Credential = New-Object System.Management.Automation.PSCredential($username, $Securestring)
+
+$PPDM=Connect-PPDMsystem -fqdn $PPDM_FQDN -trustCert -Credential $Credential
+
+$PROTECTION_JOBS=Get-PPDMactivities -PredefinedFilter PROTECTION_JOBS
+$PROTECTION_JOBS | ft | Out-String
+
+$ASSET_JOBS=Get-PPDMactivities -PredefinedFilter ASSET_JOBS
+$ASSET_JOBS | ft | Out-String
+
+$SYSTEM_JOBS=Get-PPDMactivities -PredefinedFilter SYSTEM_JOBS
+$SYSTEM_JOBS | ft | Out-String
+$SYSTEM_JOBS[0]  | Get-PPDMactivities | Out-String
+Get-PPDMactivity_metrics | Out-String
+Get-PPDMactivity_metrics -states  | Out-String
+
