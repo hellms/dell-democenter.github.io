a3a44d5df265480fb53ff90fc5b3f11cc6255e93
Karsten Bott <Karsten.Bott@bottnet.de>
2023-08-25 10:24:39 +0000

    4_1 scripted

--- scripts/Module_4_1.ps1
+++ scripts/Module_4_1.ps1
@@ -1,20 +1,17 @@
 # MODULE 4 - PROTECT SQL DATABASES
 # Scripted Version
 ## LESSON 1 - DISCOVER SQL DATABASES
-$SQLHOST="sql-02.demo.local"
+$SQL_HOSTNAME="sql-02.demo.local"
 Write-Host "Reviewing the Agent Regitration Status."
-
-
-Get-PPDMagent_registration_status | out-string
-
-
-Get-PPDMWhitelist | Set-PPDMWhitelist -state APPROVED | out-string
-Write-Host "Starting a discover for host $SQLHOST"
-
-
-Get-PPDMhosts -filter "name eq `"$SQLHOST`"" | Start-PPDMdiscoveries -level HOSTFULL -start hosts
-
-Get-PPDMactivities -taskid <Use TaskID from you discover to check the activity>
-
-Write-Host "Getting discovered Databases for Host $SQLHOST"
-Get-PPDMassets -type MICROSOFT_SQL_DATABASE -filter 'details.database.clusterName eq "sql-02.demo.local"' | ft | out-string
+Get-PPDMagent_registration_status  6>$null | out-string
+Get-PPDMWhitelist  6>$null| Set-PPDMWhitelist -state APPROVED | out-string
+Write-Host "Starting a discover for host $SQL_HOSTNAME"
+$Discoveries=Get-PPDMhosts -filter "name eq `"$SQL_HOSTNAME`""  6>$null | Start-PPDMdiscoveries -level HOSTFULL -start hosts
+do { 
+    Sleep 5;
+    $Activity=$Discoveries | Get-PPDMactivities  6>$null
+    write-host -NoNewline "$($Activity.progress)% "
+    }
+until ($Activity.state -eq "COMPLETED")
+Write-Host "Getting discovered Databases for Host $SQL_HOSTNAME"
+Get-PPDMassets -type MICROSOFT_SQL_DATABASE -filter "details.database.clusterName eq `"$SQL_HOSTNAME`""  6>$null| ft | out-string
