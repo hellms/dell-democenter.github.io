a6620ef9f50e5b17c3c3b16273ede8be0f789e41
bottkars <karsten.bott@dell.com>
2024-02-06 17:02:13 +0100

    latest

--- scripts/Module_5_2.ps1
+++ scripts/Module_5_2.ps1
@@ -1,6 +1,6 @@
-# MODULE 5 - PROTECT & RESTORE ORACLE DATABASE FROM POWERSHELL
+Write-Host "# MODULE 5 - PROTECT & RESTORE ORACLE DATABASE FROM POWERSHELL
 # Scripted Version
-## LESSON 2 - VIEW ORACLE COPIES
+## LESSON 2 - VIEW ORACLE COPIES"
 $Asset=Get-PPDMassets -type ORACLE_DATABASE -filter 'name eq "orcl"' 6>$null 
 $Asset | Get-PPDMassetcopies | ft | Out-String
 $Asset | Get-PPDMlatest_copies| Out-String
